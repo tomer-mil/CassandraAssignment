ec636f2c4d8bd524b291403b8c3dea323ff0a788
Tomer Mildworth <tomer.mildworth@gmail.com>
2025-01-21 18:56:53 +0200

    Finished reviews SELECT functions

--- src/bigdatacourse/hw2/studentcode/HW2StudentAnswer.java
+++ src/bigdatacourse/hw2/studentcode/HW2StudentAnswer.java
@@ -142,18 +142,15 @@ public class HW2StudentAnswer implements HW2API {
 	
 	@Override
 	public void createTables() {
-		//TODO: implement this function
 
 		session.execute(CQL_ITEM_VIEW_CREATE_TABLE);
 		session.execute(CQL_USER_REVIEWS_VIEW_CREATE_TABLE);
 		session.execute(CQL_ITEM_REVIEWS_VIEW_CREATE_TABLE);
 
-		System.out.println("TODO: implement this function...\nDONE!");
 	}
 
 	@Override
 	public void initialize() {
-		//TODO: implement this function
 
 		// Item prepared statements
 		this.insertItemView = session.prepare(CQL_ITEM_VIEW_INSERT);
@@ -166,7 +163,6 @@ public class HW2StudentAnswer implements HW2API {
 		// Item reviews prepared statements
 		this.insertItemReviewsView = session.prepare(CQL_ITEM_REVIEWS_VIEW_INSERT);
 		this.selectItemReviewsView = session.prepare(CQL_ITEM_REVIEWS_VIEW_SELECT);
-		System.out.println("TODO: implement this function...\nDONE!");
 	}
 
 	@Override
@@ -201,12 +197,10 @@ public class HW2StudentAnswer implements HW2API {
 		}
 		executor.shutdown();
 		br.close();
-		System.out.println("TODO: implement this function...");
 	}
 
 	@Override
 	public void loadReviews(String pathReviewsFile) throws Exception {
-		//TODO: implement this function
 		BufferedReader br = getBufferedReader(pathReviewsFile);
 		String line;
 
@@ -242,13 +236,10 @@ public class HW2StudentAnswer implements HW2API {
 		}
 		executor.shutdown();
 		br.close();
-		System.out.println("TODO: implement this function...");
-		System.out.println("DONE!");
 	}
 
 	@Override
 	public String item(String asin) {
-		//TODO: implement this function
 		String item;
 		Row itemSelectResult = selectItemView(asin);
 		if (itemSelectResult != null) {
@@ -275,8 +266,6 @@ public class HW2StudentAnswer implements HW2API {
 //				new TreeSet<String>(Arrays.asList("Notebooks & Writing Pads", "Office & School Supplies", "Office Products", "Paper")),
 //				"Circa + Behance = Productivity. The minute-to-minute flexibility of Circa note-taking meets the organizational power of the Action Method by Behance. The result is enhanced productivity, so you'll formulate strategies and achieve objectives even more efficiently with this Circa notebook and project planner. Read Steve's blog on the Behance/Levenger partnership Customize with your logo. Corporate pricing available. Please call 800-357-9991."
 //			);
-		System.out.println("TODO: implement this function...");
-		System.out.println("DONE!");
 		return item;
 	}
 	
@@ -284,83 +273,109 @@ public class HW2StudentAnswer implements HW2API {
 	@Override
 	public Iterable<String> userReviews(String reviewerID) {
 		// the order of the reviews should be by the time (desc), then by the asin
-		//TODO: implement this function
-		System.out.println("TODO: implement this function...");
-		
+		ResultSet userReviewsSelectResults = selectUserReviewsView(reviewerID);
+		ArrayList<String> reviewReprs = new ArrayList<String>();
+		Row userReview;
+		while ((userReview = userReviewsSelectResults.one()) != null) {
+			String reviewRepr = formatReview(
+					userReview.getInstant("time"),
+					userReview.getString("asin"),
+					userReview.getString("reviewerID"),
+					userReview.getString("reviewName"),
+					userReview.getInt("rating"),
+					userReview.getString("summary"),
+					userReview.getString("reviewText")
+			);
+			reviewReprs.add(reviewRepr);
+		}
+
 		// required format - example for reviewerID A17OJCRPMYWXWV
-		ArrayList<String> reviewRepers = new ArrayList<String>();
-		String reviewRepr1 = formatReview(
-			Instant.ofEpochSecond(1362614400),
-			"B005QDG2AI",
-			"A17OJCRPMYWXWV",
- 			"Old Flour Child",
-			5,
-			"excellent quality",
-			"These cartridges are excellent .  I purchased them for the office where I work and they perform  like a dream.  They are a fraction of the price of the brand name cartridges.  I will order them again!"
-		);
-		reviewRepers.add(reviewRepr1);
+//		String reviewRepr1 = formatReview(
+//			Instant.ofEpochSecond(1362614400),
+//			"B005QDG2AI",
+//			"A17OJCRPMYWXWV",
+// 			"Old Flour Child",
+//			5,
+//			"excellent quality",
+//			"These cartridges are excellent .  I purchased them for the office where I work and they perform  like a dream.  They are a fraction of the price of the brand name cartridges.  I will order them again!"
+//		);
+//		reviewRepers.add(reviewRepr1);
+//
+//		String reviewRepr2 = formatReview(
+//			Instant.ofEpochSecond(1360108800),
+//			"B003I89O6W",
+//			"A17OJCRPMYWXWV",
+//			"Old Flour Child",
+//			5,
+//			"Checkbook Cover",
+//			"Purchased this for the owner of a small automotive repair business I work for.  The old one was being held together with duct tape.  When I saw this one on Amazon (where I look for almost everything first) and looked at the price, I knew this was the one.  Really nice and very sturdy."
+//		);
+//		reviewRepers.add(reviewRepr2);
 
-		String reviewRepr2 = formatReview(
-			Instant.ofEpochSecond(1360108800),
-			"B003I89O6W",
-			"A17OJCRPMYWXWV",
-			"Old Flour Child",
-			5,
-			"Checkbook Cover",
-			"Purchased this for the owner of a small automotive repair business I work for.  The old one was being held together with duct tape.  When I saw this one on Amazon (where I look for almost everything first) and looked at the price, I knew this was the one.  Really nice and very sturdy."
-		);
-		reviewRepers.add(reviewRepr2);
-
-		System.out.println("total reviews: " + 2);
-		return reviewRepers;
+//		System.out.println("total reviews: " + 2);
+		return reviewReprs;
 	}
 
 	@Override
 	public Iterable<String> itemReviews(String asin) {
 		// the order of the reviews should be by the time (desc), then by the reviewerID
-		//TODO: implement this function
-		System.out.println("TODO: implement this function...");
+		ResultSet itemReviewsSelectResults = selectItemReviewsView(asin);
+		ArrayList<String> reviewReprs = new ArrayList<String>();
+		Row itemReview;
+
+		while ((itemReview = itemReviewsSelectResults.one()) != null) {
+			String reviewRepr = formatReview(
+					itemReview.getInstant("time"),
+					itemReview.getString("asin"),
+					itemReview.getString("reviewerID"),
+					itemReview.getString("reviewerName"),
+					itemReview.getInt("rating"),
+					itemReview.getString("summary"),
+					itemReview.getString("reviewText")
+			);
+			reviewReprs.add(reviewRepr);
+		}
 		
 		// required format - example for asin B005QDQXGQ
-		ArrayList<String> reviewRepers = new ArrayList<String>();
-		reviewRepers.add(
-			formatReview(
-				Instant.ofEpochSecond(1391299200),
-				"B005QDQXGQ",
-				"A1I5J5RUJ5JB4B",
-				"T. Taylor \"jediwife3\"",
-				5,
-				"Play and Learn",
-				"The kids had a great time doing hot potato and then having to answer a question if they got stuck with the &#34;potato&#34;. The younger kids all just sat around turnin it to read it."
-			)
-		);
-
-		reviewRepers.add(
-			formatReview(
-				Instant.ofEpochSecond(1390694400),
-				"B005QDQXGQ",
-				"\"AF2CSZ8IP8IPU\"",
-				"Corey Valentine \"sue\"",
-				1,
-			 	"Not good",
-				"This Was not worth 8 dollars would not recommend to others to buy for kids at that price do not buy"
-			)
-		);
-		
-		reviewRepers.add(
-			formatReview(
-				Instant.ofEpochSecond(1388275200),
-				"B005QDQXGQ",
-				"A27W10NHSXI625",
-				"Beth",
-				2,
-				"Way overpriced for a beach ball",
-				"It was my own fault, I guess, for not thoroughly reading the description, but this is just a blow-up beach ball.  For that, I think it was very overpriced.  I thought at least I was getting one of those pre-inflated kickball-type balls that you find in the giant bins in the chain stores.  This did have a page of instructions for a few different games kids can play.  Still, I think kids know what to do when handed a ball, and there's a lot less you can do with a beach ball than a regular kickball, anyway."
-			)
-		);
-
-		System.out.println("total reviews: " + 3);
-		return reviewRepers;
+//		ArrayList<String> reviewRepers = new ArrayList<String>();
+//		reviewRepers.add(
+//			formatReview(
+//				Instant.ofEpochSecond(1391299200),
+//				"B005QDQXGQ",
+//				"A1I5J5RUJ5JB4B",
+//				"T. Taylor \"jediwife3\"",
+//				5,
+//				"Play and Learn",
+//				"The kids had a great time doing hot potato and then having to answer a question if they got stuck with the &#34;potato&#34;. The younger kids all just sat around turnin it to read it."
+//			)
+//		);
+//
+//		reviewRepers.add(
+//			formatReview(
+//				Instant.ofEpochSecond(1390694400),
+//				"B005QDQXGQ",
+//				"\"AF2CSZ8IP8IPU\"",
+//				"Corey Valentine \"sue\"",
+//				1,
+//			 	"Not good",
+//				"This Was not worth 8 dollars would not recommend to others to buy for kids at that price do not buy"
+//			)
+//		);
+//
+//		reviewRepers.add(
+//			formatReview(
+//				Instant.ofEpochSecond(1388275200),
+//				"B005QDQXGQ",
+//				"A27W10NHSXI625",
+//				"Beth",
+//				2,
+//				"Way overpriced for a beach ball",
+//				"It was my own fault, I guess, for not thoroughly reading the description, but this is just a blow-up beach ball.  For that, I think it was very overpriced.  I thought at least I was getting one of those pre-inflated kickball-type balls that you find in the giant bins in the chain stores.  This did have a page of instructions for a few different games kids can play.  Still, I think kids know what to do when handed a ball, and there's a lot less you can do with a beach ball than a regular kickball, anyway."
+//			)
+//		);
+//
+//		System.out.println("total reviews: " + 3);
+		return reviewReprs;
 	}
 
 	
@@ -397,7 +412,7 @@ public class HW2StudentAnswer implements HW2API {
 	}
 
 	private JSONObject getTableJSONObject(String stringJSON, boolean isItem) {
-		JSONObject withNaJSON = this.addNaToJSON(stringJSON, false);
+		JSONObject withNaJSON = this.addNaToJSON(stringJSON, isItem);
 		JSONObject parsedJSON = new JSONObject();
         if (isItem) {
             parsedJSON.put("asin", withNaJSON.get("asin"));
@@ -508,6 +523,12 @@ public class HW2StudentAnswer implements HW2API {
 		session.execute(insertUserReviewsBstmt);
 	}
 
+	private ResultSet selectUserReviewsView(String reviewerID) {
+		BoundStatement selectUserReviewsBstmt = selectUserReviewsView.bind()
+				.setString(0, reviewerID);
+		return session.execute(selectUserReviewsBstmt);
+	}
+
 	private void insertItemReviewsView(String asin, Instant time, String reviewerID, String reviewerName, int rating, String summary, String reviewText) {
 		BoundStatement insertItemReviewsBstmt = insertItemReviewsView.bind()
 						.setString("asin", asin)
@@ -518,5 +539,11 @@ public class HW2StudentAnswer implements HW2API {
 						.setString("summary", summary)
 						.setString("reviewText", reviewText);
 		session.execute(insertItemReviewsBstmt);
+	}
+
+	private ResultSet selectItemReviewsView(String asin) {
+		BoundStatement selectItemReviewsBstmt = selectItemReviewsView.bind()
+				.setString(0, asin);
+		return session.execute(selectItemReviewsBstmt);
 	}
 }
